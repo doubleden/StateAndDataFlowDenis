caf14fc893748ad1e2eab56c5539a9ec17a66209
Denis Denisov <denis.denisov8701@gmail.com>
2024-04-23 22:31:44 +0200

    created stotage manager

---
+++ StateAndDataFlow/Services/StorageManager.swift
@@ -0,0 +1,12 @@
+//
+//  StorageManager.swift
+//  StateAndDataFlow
+//
+//  Created by Denis Denisov on 23/4/24.
+//
+
+final class StorageManager {
+    let shared = StorageManager()
+    
+    private init() {}
+}
